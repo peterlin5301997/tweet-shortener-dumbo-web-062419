319f758cfaec6c45e64ee5f6ba4785fc579b3652
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 21:03:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,7 +4,7 @@ def dictionary
     "hello" => "hi",
     "to" => "2", "two" => "2", "too" => "2",
     "for" => "4", "four" => "4",
-    "be" => "b"
+    "be" => "b",
     "you" => "u",
     "at" => "@",
     "and" => "&"
