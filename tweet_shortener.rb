bd6376d607c05cff63406e97ce2d639ffad7816c
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 20:51:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,7 @@ def word_substituter(string)
   end
   strWords = ""
   arrWords.each do |word|
-    if word == word.last
+    if word == arrWords.last
       strWords += word
     else
       strWords += word + " "
