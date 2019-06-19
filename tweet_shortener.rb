c0b64b0c09037ab0e04f9227449fc4afb34bdabd
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 20:44:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4"
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  arrWords = string.split
+end
